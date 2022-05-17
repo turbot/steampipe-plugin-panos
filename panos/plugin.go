25a8fe405efe21f63a59c6cf4ad20a48e956b2d4
Binaek Sarkar <binaek@turbot.com>
2022-05-17 13:22:57 +0530

    correct table reference

--- panos/plugin.go
+++ panos/plugin.go
@@ -19,10 +19,10 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			ShouldIgnoreError: isNotFoundError,
 		},
 		TableMap: map[string]*plugin.Table{
-			"panos_address_object": tablePanosAddressObject(ctx),
-			"panos_nat_rule":       tablePanosNATRule(ctx),
-			"panos_security_rule":  tablePanosSecurityRule(ctx),
-			"panos_tag_object":     tablePanosTagObject(ctx),
+			"panos_address_object":     tablePanosAddressObject(ctx),
+			"panos_nat_rule":           tablePanosNATRule(ctx),
+			"panos_security_rule":      tablePanosSecurityRule(ctx),
+			"panos_administrative_tag": tablePanosTagObject(ctx),
 		},
 	}
 	return p
